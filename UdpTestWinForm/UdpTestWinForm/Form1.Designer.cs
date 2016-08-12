f1083b17c2a7d374a5c545197ce8a40619436a90
colinchristo4 <colin.christo4@gmail.com>
2016-08-12 16:02:21 -0700

    Added GhettoGUI For testing
    ghetto af

---
+++ UdpTestWinForm/UdpTestWinForm/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace UdpTestWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tb_IP = new System.Windows.Forms.TextBox();
+            this.tb_Port = new System.Windows.Forms.TextBox();
+            this.tb_Message = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.tb_ExitPort = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btn_StopTimer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_IP
+            // 
+            this.tb_IP.Location = new System.Drawing.Point(84, 99);
+            this.tb_IP.Name = "tb_IP";
+            this.tb_IP.Size = new System.Drawing.Size(100, 20);
+            this.tb_IP.TabIndex = 0;
+            // 
+            // tb_Port
+            // 
+            this.tb_Port.Location = new System.Drawing.Point(84, 138);
+            this.tb_Port.Name = "tb_Port";
+            this.tb_Port.Size = new System.Drawing.Size(100, 20);
+            this.tb_Port.TabIndex = 1;
+            // 
+            // tb_Message
+            // 
+            this.tb_Message.Location = new System.Drawing.Point(84, 188);
+            this.tb_Message.Name = "tb_Message";
+            this.tb_Message.Size = new System.Drawing.Size(103, 20);
+            this.tb_Message.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Message";
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Location = new System.Drawing.Point(84, 243);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(75, 23);
+            this.btn_Send.TabIndex = 6;
+            this.btn_Send.Text = "Send";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // tb_ExitPort
+            // 
+            this.tb_ExitPort.Location = new System.Drawing.Point(204, 138);
+            this.tb_ExitPort.Name = "tb_ExitPort";
+            this.tb_ExitPort.Size = new System.Drawing.Size(100, 20);
+            this.tb_ExitPort.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(201, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Exit Port";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btn_StopTimer
+            // 
+            this.btn_StopTimer.Location = new System.Drawing.Point(204, 243);
+            this.btn_StopTimer.Name = "btn_StopTimer";
+            this.btn_StopTimer.Size = new System.Drawing.Size(75, 23);
+            this.btn_StopTimer.TabIndex = 9;
+            this.btn_StopTimer.Text = "Stop Timer";
+            this.btn_StopTimer.UseVisualStyleBackColor = true;
+            this.btn_StopTimer.Click += new System.EventHandler(this.btn_StopTimer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 332);
+            this.Controls.Add(this.btn_StopTimer);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb_ExitPort);
+            this.Controls.Add(this.btn_Send);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_Message);
+            this.Controls.Add(this.tb_Port);
+            this.Controls.Add(this.tb_IP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_IP;
+        private System.Windows.Forms.TextBox tb_Port;
+        private System.Windows.Forms.TextBox tb_Message;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.TextBox tb_ExitPort;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btn_StopTimer;
+    }
+}
+
---
+++ UdpTestWinForm/UdpTestWinForm/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UdpTestWinForm
+{
+    public partial class Form1 : Form
+    {
+        private IPAddress addr = null;
+        private int port = -1;
+        private int exitPort = -1;
+        private UdpClient client = null;
+        private UdpClient exitClient = null;
+        private int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfigureClients()
+        {
+            client = new UdpClient(port);
+            exitClient = new UdpClient(exitPort);
+
+            client.Connect(addr, port);
+            exitClient.Connect(addr, exitPort);
+        }
+
+        private void CloseClients()
+        {
+            if (client != null)
+            {
+                client.Close();
+            }
+            if (exitClient != null)
+            {
+                exitClient.Close();
+            }
+        }
+
+        private void btn_Send_Click(object sender, EventArgs e)
+        {
+            if ((addr == null) || (port == -1) || (exitPort == -1))
+            {
+                if (!(IPAddress.TryParse(tb_IP.Text, out addr)))
+                {
+                    tb_IP.Text = "Try Again";
+                    return;
+                }
+
+                if (!(int.TryParse(tb_Port.Text, out port)))
+                {
+                    tb_Port.Text = "Try Again.";
+                    return;
+                }
+
+                if (!(int.TryParse(tb_ExitPort.Text, out exitPort)))
+                {
+                    tb_ExitPort.Text = "Try Again.";
+                    return;
+                }
+
+                ConfigureClients();
+            }
+
+            if (string.IsNullOrEmpty(tb_Message.Text))
+                return;
+
+            try
+            {
+
+                if (tb_Message.Text == "exit")
+                {
+                    byte[] toSend = Encoding.UTF8.GetBytes("exit");
+                    exitClient.Send(toSend, toSend.Length);
+                }
+                else if (tb_Message.Text == "data")
+                {
+                    timer1.Enabled = true;
+                    timer1.Start();
+                }
+                else
+                {
+                    byte[] toSend = Encoding.UTF8.GetBytes(tb_Message.Text);
+                    client.Send(toSend, toSend.Length);
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string str = i.ToString();
+
+            byte[] bs = Encoding.UTF8.GetBytes(str);
+
+            client.Send(bs, bs.Length);
+
+            if (i == 100)
+                i = 0;
+
+            i++;
+        }
+
+        private void btn_StopTimer_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tb_Message.Text = "lol";
+            tb_ExitPort.Text = "7979";
+            tb_Port.Text = "6969";
+            tb_IP.Text = "192.168.1.227";
+
+            timer1.Interval = 100;
+        }
+    }
+}
